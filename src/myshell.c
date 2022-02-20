a7488b3544556b2adc12a1e21ac857af51449541
Gareth Hogan <gareth.hogan22@mail.dcu.ie>
2022-02-20 12:42:02 +0000

    Added lots of comments to shell program

--- src/myshell.c
+++ src/myshell.c
@@ -27,78 +27,82 @@ void command_select(char **args);
 
 int main(int argc, char*argv[])
 {
-    char * args[100];
-    char ** arg;
+    char * args[100]; // set up space to store arguments
+    char ** arg; // pointer into args array
     char buff[100];
-    int batch = 1;
+    int batch = 1; //signal variable default = no batch
     
-    char cwd[256];
-    getcwd(cwd,sizeof(cwd));
+    char cwd[256]; // variable to store cwd
+    getcwd(cwd,sizeof(cwd)); // get starting cwd
 
-    FILE *fp = stdin;
-    if(argv[1]){
-        batch = 0;
-        fp = batchfile(argv[1]);
+    FILE *fp = stdin; //default file pointer for stdin
+    if(argv[1]){ // if there is an argument after invocation of shell i.e. a batchfile
+        batch = 0; //signal variable changes to say yes there is a batch file
+        fp = batchfile(argv[1]); //invokes function while will overwrite stdin file pointer
     }
     
-    while(!feof(fp)){
+    while(!feof(fp)){ // while having not reached end of file of input file, stdin or batch
         pid_t pid;
 
-        if(batch == 1){
+        if(batch == 1){ // if there is no batch file print prompt
             prompt(cwd);
         }
-        if(fgets(buff, sizeof(buff), (FILE*)fp))
+        if(fgets(buff, sizeof(buff), (FILE*)fp)) // if able to get a line from user/batch
         {
 
             arg = args;
-            *arg++ = strtok(buff, SEPERATORS);
-            while((*arg++ = strtok(NULL,SEPERATORS)));
+            *arg++ = strtok(buff, SEPERATORS); // split 1st argument from line by whitespace
+            while((*arg++ = strtok(NULL,SEPERATORS))); // add all args to array, splitting as goes
 
-            int waitint = backgroundcheck(args);
+            int waitint = backgroundcheck(args); // invokes funtion to check for & after program
             
-            switch(pid = fork())
+            switch(pid = fork()) // main fork to send child off to do stuff
             {
-                case -1:
+                case -1: // case for if an error occurs when forking
                     errorhandler("fork to run command selector.");
                     break;
-                case 0: ; //silly little semicolon fixes declaring and assigning in a switch
+                case 0: ; // case for the child, here is what the child will do
                     int check = filecheck(args);
-                    //puts("HELLO?");
+                    //puts("HELLO? (from puts)");
+                    //printf("Hello? (From printf)\n");
+                    //puts("WHat da frick?");
                     if(check == 0){
                         command_select(args);
                     }
-                    pid_t childpid = getpid();
-                    kill(childpid, SIGTERM);
+                    //puts("After command? (from puts)");
+                    //printf("after? (From printf)\n");
+                    pid_t childpid = getpid(); //get pid of child
+                    kill(childpid, SIGTERM); // kill the child after it is finished, prevents children spawning children
                     break;
-                default:
-                    if(waitint)
+                default: // what the parent process will do
+                    if(waitint) // if the parent should wait
                     {
                         waitpid(pid, NULL, WUNTRACED);
                     }
-                    else
+                    else // if the parent shouldn't wait, i.e. for a background process
                     {
                         waitpid(pid, NULL, WNOHANG);
                         sleep(1);
                     }
                     waitint = 1;
             }
-            getcwd(cwd, sizeof(cwd));
+            getcwd(cwd, sizeof(cwd)); //refresh the stored cwd in case of updates to it (i.e. cd)
         }
     }
-    fclose(fp);
+    fclose(fp); //closes the batchfile
 }
 
-void prompt(char* cwd)
+void prompt(char* cwd) //function to print the prompt in cyan
 {
     printf("\033[0;36m");
     printf("%s >>> ",cwd);
     printf("\033[0m");
 }
 
-FILE* batchfile(char * file)
+FILE* batchfile(char * file) //batchfile checking function, returns update file pointer if there is one.
 {
     FILE* fp;
-    switch(access(file, F_OK))
+    switch(access(file, F_OK)) // checks if file exists
     {
         case -1:
             printf("\033[0;31m");
@@ -106,28 +110,74 @@ FILE* batchfile(char * file)
             printf("\033[0m");
             exit(1);
         case 0:
-            fp = fopen(file, "r");
+            fp = fopen(file, "r"); // open batch file in reading mode
     }
-    return fp;
+    return fp; // return updated fp 
 }
 
-int backgroundcheck(char ** args)
+int backgroundcheck(char ** args) // function to check for &
 {
     int position = 0;
-    while(args[position + 1] != NULL)
+    while(args[position + 1] != NULL) // checks index of last arg but iterating until end
     {
         position++;
     }
-    if(strcmp(args[position], "&"))
+    if(strcmp(args[position], "&")) // if last argument is an &
     {
-        return 1;
+        return 1;  // return signal to say don't wait
     }
-    return 0;
+    return 0; // else keep signal yes for waiting
 }
 
-void command_select(char ** args)
+int filecheck(char ** args) // big function to check for input and output files within args
 {
-    if(strcmp(args[0],"quit") == 0){
+    int i = 0; // variable to track index as we iterate through args
+    int input = -1; // default is no file for input or output, signalled by -1
+    int output = -1;
+    while(args[i] != NULL){ // iterate through args
+        if(strcmp(args[i], "<") == 0) //check for input signal
+        {
+            input = i + 1; // if find < the file is the arg after that
+        }
+        if((strcmp(args[i], ">") == 0) || (strcmp(args[i], ">>") == 0)) // similar for output
+        {
+            output = i + 1;
+        }
+        i++; //increment i through args
+    }
+    if(input != -1) // if there was a file found
+    {
+        switch(access(args[input], F_OK)) // check access to input file
+        {
+            case -1: // error, no such file
+                printf("\033[0;31m");
+                printf("Error: No such file %s exists\n", args[input]);
+                printf("\033[0m");
+                return 1;
+            case 0: // if file is fine
+                //printf("Input file is %s\n", args[input]);
+                freopen(args[input], "r", stdin); // open file replacing stdin
+        }
+    }
+    if(output != -1) // if there was an output file found
+    {
+        if(strcmp(args[output - 1], ">") == 0) // truncating 
+        {
+            printf("Opened %s for output truncating\n", args[output]);
+            freopen(args[output], "w", stdout); //open in w mode
+        }
+        else if(strcmp(args[output - 1], ">>") == 0) // appending
+        {
+            printf("Opened %s for output appending\n", args[output]);
+            freopen(args[output], "a", stdout); // open in a mode
+        }
+    }
+    return 0; // return signal saying all is fine
+}
+
+void command_select(char ** args) // big switch case function to test if given command is internal
+{
+    if(strcmp(args[0],"quit") == 0){ // quit command
         printf("QUIT COMMAND RECEIVED\n");
         exit(0);
     }
@@ -156,130 +206,84 @@ void command_select(char ** args)
         pauseshell();
         return;
     }
-    else{
+    else{ // if it matches none of our internal commands, send it to function for external commands
         external_command(args);   
         return;        
     }
 }
 
-int filecheck(char ** args)
-{
-    int i = 0;
-    int input = -1;
-    int output = -1;
-    while(args[i] != NULL){
-        if(strcmp(args[i], "<") == 0)
-        {
-            input = i + 1;
-        }
-        if((strcmp(args[i], ">") == 0) || (strcmp(args[i], ">>") == 0))
-        {
-            output = i + 1;
-        }
-        i++;
-    }
-    if(input != -1)
-    {
-        switch(access(args[input], F_OK)) // input file
-        {
-            case -1:
-                printf("\033[0;31m");
-                printf("Error: No such file %s exists\n", args[input]);
-                printf("\033[0m");
-                return 1;
-            case 0:
-                //printf("Input file is %s\n", args[input]);
-                freopen(args[input], "r", stdin);
-        }
-    }
-    if(output != -1)
-    {
-        if(strcmp(args[output - 1], ">") == 0)
-        {
-            printf("Opened %s for output truncating\n", args[output]);
-            freopen(args[output], "w", stdout);
-        }
-        else if(strcmp(args[output - 1], ">>") == 0)
-        {
-            printf("Opened %s for output appending\n", args[output]);
-            freopen(args[output], "a", stdout);
-        }
-    }
-    return 0;
-}
-
 void clear()
 {
-    execlp("clear","clear",NULL);
+    execlp("clear","clear",NULL); // exec clear to clear screen
     return;
 }
 
 void dir(char ** args)
 {
-    char base[] = "ls";
-    char defaultarg[] = "-al";
+    char base[] = "ls"; // base command for dir is ls
+    char defaultarg[] = "-al"; // default dir should always use -al
     execlp(base,base,defaultarg,args[1],NULL);
     return;
 }
 
-void cd(char ** args)
+void cd(char ** args) // cd command
 {
-    char cwd[256];
-    getcwd(cwd,sizeof(cwd));
-    if(!args[1])
+    char cwd[256]; // holder for cwd
+    getcwd(cwd,sizeof(cwd)); // get cwd
+    if(!args[1]) // if there is no argument, print cwd
     {
         printf("Current directory is: %s\n", cwd);
         return;
     }
-    chdir(args[1]);
-    getcwd(cwd,sizeof(cwd));
-    char newpwd[] = "PWD=";
-    strcat(newpwd, cwd);
-    putenv(strdup(newpwd));
+    chdir(args[1]); // change directory
+    getcwd(cwd,sizeof(cwd)); // get new cwd
+    char newpwd[] = "PWD="; // start of environ string
+    strcat(newpwd, cwd); // append cwd to PWD= to create environ string
+    putenv(strdup(newpwd)); // update environ string for PWD
     return;
 }
 
-void getenviron()
+void getenviron() // print our environ strings
 {
     int count = 0;
-    while(environ[count] != NULL)
+    while(environ[count] != NULL) // iterate through external array containing env strings
     {
-        puts(environ[count]);
-        count++;
+        puts(environ[count]); // print them out
+        count++; //iterate
     }
     return;
 }
 
-void echo(char ** args)
+void echo(char ** args) // internal echo command
 {
-    char string[100] = "";
-    int i = 1;
-    while(args[i] != NULL)
+    char string[100] = ""; // base string to add all args to
+    int i = 1; // starting index for iterating thru args
+    while(args[i] != NULL) //iterate
     {
-        strcat(string, args[i]);
+        strcat(string, args[i]); //add args to string + a space inbetween
         strcat(string, " ");
         i++;
     }
     printf("\033[0;33m");
-    printf("%s\n", string);
+    printf("%s\n", string); //print the string in yellow
     printf("\033[0m");
     return;
 }
 
-void pauseshell()
+void pauseshell() // pause shell
 {
     printf("\033[0;32m");
     printf("Press [Enter] key to continue.");
     printf("\033[0m");
-    getchar();
+    getchar(); // waits until user presses enter
     return;
 }
 
-void external_command(char ** args)
+void external_command(char ** args) // for any unrecognised internal command, push to OS 
 {
     int error;
-    error = execvp(args[0],args);
-    if(error == -1)
+    error = execvp(args[0],args); // exevp the whole array of args, returns a code
+    if(error == -1) // if code signals an error, tell the user
     {
             printf("\033[0;31m");
             printf("Unknown command, try another.\n");
@@ -288,7 +292,7 @@ void external_command(char ** args)
     return;
 }
 
-void errorhandler(char * call)
+void errorhandler(char * call) // error handler function
 {
     printf("\033[0;31m");
     printf("Error when executing %s\n", call);
